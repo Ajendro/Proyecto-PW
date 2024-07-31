eeba5c16d8da3d84da5b415a9968305e94317450
Jair <141942836+Ajendro@users.noreply.github.com>
2024-07-31 16:12:13 -0500

    Frontend

---
+++ Marketplace_Front/marketplace/src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+    <div>
+    <Footer/>
+    </div>
+    </div>
+  )
+}
