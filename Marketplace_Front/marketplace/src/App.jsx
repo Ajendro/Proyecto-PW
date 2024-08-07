5a452711f1d9b617db568882758670848e854cf9
gariosfe <gariosfe@uide.edu.ec>
2024-08-07 16:56:13 -0500

    a
    a

--- Marketplace_Front/marketplace/src/App.jsx
+++ Marketplace_Front/marketplace/src/App.jsx
@@ -1,31 +1,12 @@
-
 import { BrowserRouter } from "react-router-dom";
 import Rutes from "./rutes";
-
-
 
 export default function App() {
   return (
     <div>
-<<<<<<< Updated upstream
       <BrowserRouter>
         <Rutes />
       </BrowserRouter>
     </div>
   );
-=======
-    <Example />
-    
-    <div style={{marginTop:"150px", marginLeft:"200px" }}>
-    <CategoriaPro/>
-    </div>
-    <Products/>
-    <Form />
-    
-    <Footer />
-    </div>    
-   </div>
-  )
-
->>>>>>> Stashed changes
 }
