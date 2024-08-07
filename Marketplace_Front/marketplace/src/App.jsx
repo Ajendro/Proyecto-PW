376d1a257fbce90801fc0d9089ad5d3c5c73b7c9
Jair <141942836+Ajendro@users.noreply.github.com>
2024-08-07 17:07:03 -0500

    cambio

--- Marketplace_Front/marketplace/src/App.jsx
+++ Marketplace_Front/marketplace/src/App.jsx
@@ -9,5 +9,7 @@ export default function App() {
         <Rutes />
       </BrowserRouter>
     </div>
+
+    
   );
 }
--- Marketplace_Front/marketplace/src/rutes.jsx
+++ Marketplace_Front/marketplace/src/rutes.jsx
@@ -2,18 +2,31 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Example from "./components/carrusel";
-
-
+import Product from "./components/products";
+import Categoria from "./components/categoriaPro";
 import Form from "./components/Form";
 
 const Formpages = () => {
   return (
     <div>
       <Header />
-        <Routes>
-            <Route path="/" element={<Example />} />
-            <Route path="/form" element={<Form />} />
-        </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Example />
+              <div style={{marginTop:"150px", marginLeft:"150px"}}>
+              <Categoria/>
+              </div>
+              
+              <Product /> 
+            </div>
+          }
+        />
+        <Route path="/form" element={<Form />} />
+        
+      </Routes>
       <Footer />
     </div>
   );
